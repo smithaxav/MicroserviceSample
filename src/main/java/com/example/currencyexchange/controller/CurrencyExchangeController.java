4dbb6b03b3207c4613a5aee1f89e0bb833ba282d
Smitha Xavier <smitha.xavier@gmail.com>
2023-08-01 13:52:43 +0530

    'draft'

---
+++ src/main/java/com/example/currencyexchange/controller/CurrencyExchangeController.java
@@ -0,0 +1,31 @@
+package com.example.currencyexchange.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.currencyexchange.vo.CurrencyExchange;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to) {
+		
+		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to , BigDecimal.valueOf(50));
+		currencyExchange.setEnvironment(environment.getProperty("server.port"));
+		return currencyExchange;
+		
+	}
+	
+
+}
